2aafec897f19743ea81fe9a809f0b75f4973740e
Primanta Holand Bangun <38172009+primantah@users.noreply.github.com>
2020-05-25 11:52:50 +0700

    Update cov19.m

--- cov19.m
+++ cov19.m
@@ -1,18 +1,19 @@
-%% VISUALIZATION REMARKABLE DATE AND NUMBER OF SICK DUE TO COV-19 %%
-%% ----- untuk kalangan sendiri ----- %%
+%% VISUALIZATION NUMBER OF SICK DUE TO COV-19
 
 clear
 close all
 
 %% PARAMETER TANGGAL
-d1 = '01Mar2020'; %% hari ke-0 covid muncul di indo
-d2 = '17Apr2020'; %% hari peak of new cases of covid di indo
-dForm = 'ddmmmyyyy';
-t = abs(datenum(d1,dForm) - datenum(d2,dForm));
+d1 = '01 Mar 2020';   % hari ke-0 covid muncul di indo
+d2 = '23 May 2020';   % hari peak of new cases of covid di indo
+d6 = '24 May 2020';   % hari ini hehe
+dForm = 'dd mmm yyyy';
+t = abs(datenum(d1,dForm) - datenum(d2,dForm)); 
+
 
 %% GRAFIK INFECTED CASES
-a = 0.1069; %% y = (a) x^(b) BENTUK UMUM DARI EXPONEN
-b = 2.7638;
+a = 0.10066; %% y = (a) x^(b) BENTUK UMUM DARI EXPONEN
+b = 2.79430;
 m = ((a)*t.^(b)); % mencari m (salah satu parameter transformasi)
 x1a = [1:1:t];
 x1c = [t:1:2*t];
@@ -21,9 +22,9 @@ y1c = (2*m)-a*((2*t)-x1c).^(b); %% y1 dicerminkan 2x, kesamping dan ke atas
 y1aEnd = a*t.^(b); %ini berguna buat menentukan titik pencerminan grafik solved
 
 %% GRAFIK SOLVED CASES
-c = 0.0009; %% y = (c) x^(d) BENTUK UMUM DARI EXPONEN
-d = 3.64061;
-y2a = c*x1a.^(d);
+c = 0.00127; %% y = (c) x^(d) BENTUK UMUM DARI EXPONEN
+d = 3.52371;
+y2a = c*x1a.^(d);                   % Grafik bagian 1
 t2b = ((y1aEnd/c).^(1/d)); % nyari waktu dimana mencapai titik balik global
 x2b = [t:1:t2b];
 y2b = c*x2b.^(d);
@@ -37,70 +38,77 @@ y6 = m*2;
 
 %% PENGGAMBARAN
 hold on;
-plot(x1a,y1a);      % mencetak grafik origin
+plot(x1a,y1a);  % mencetak grafik origin
 hold on;
-plot(x1c,y1c);      % mencetak grafik origin kebalik
-hold on;
-plot(x1a,y2a);      % mencetak grafik solved
-hold on;
-plot(x2b,y2b);      % grafik solved interpolated
-hold on;
-plot(x2c,y2c);      % grafik solved terbalik
-hold on;
-plot(x6,y6);        % extended linear grafik infected case
+plot(x1c,y1c);  % mencetak grafik origin kebalik
 hold on;
 
-plot(x1a,(y1a-y2a));    % grafik sick a
+plot(x1a,y2a);  % mencetak grafik solved
+hold on;
+plot(x2b,y2b);  % grafik solved interpolated
+hold on;
+plot(x2c,y2c);  % grafik solved terbalik
+    positivePeak = max(y2c);
+
 hold on;
 
-    y1cNew = (2*m)-a*((2*t)-x2b).^(b); % grafik y1c khusus buat pengurangan
-plot(x2b,(y1cNew-y2b));    % grafik sick b
+plot(x6,y6);    % extended linear grafik infected case
 hold on;
+
+plot(x1a,(y1a-y2a));        % grafik sick a
+hold on;
+
+    y1cNew = (2*m)-a*((2*t)-x2b).^(b);      % grafik y1c khusus buat pengurangan
+    yPeak = y1cNew-y2b;
+    plot(x2b,yPeak);                     % grafik sick b
     
-    x2cNew = [t2b:1:2*t]; %x2c khusus untuk pengurangan
-    y1cNew2 = (2*m)-a*((2*t)-x2cNew).^(b); % grafik y1c khusus buat pengurangan
+    sickPeak = max(yPeak);
+    indexPeak = find (yPeak == sickPeak);
+    d3 = x2b(indexPeak); % hari inf peak
+    d4 = d3+7;              % hari ke PSBB kelar
+    
+hold on;
+
+    x2cNew = [t2b:1:2*t];                   %x2c khusus untuk pengurangan
+    y1cNew2 = (2*m)-a*((2*t)-x2cNew).^(b);  % grafik y1c khusus buat pengurangan
     y2cNew2 = (2*m) - (c*((t4)-x2cNew).^(d));
-plot(x2cNew,(y1cNew2-y2cNew2));    % grafik sick c 
+
+plot(x2cNew,(y1cNew2-y2cNew2));             % grafik sick c 
 hold on;
 
     y2cNew3 = (2*m) - (c*((t4)-x6).^(d));
+
 plot(x6,(y6-y2cNew3));
+
+    infMin = min(y6-y2cNew3);
+    indexInfMin = find((y6-y2cNew3) == infMin);
+    d5 = x6(indexInfMin); % hari inf Min
+
 hold on;
 
-% END OF CODE -----------------------------------------------------------------
+%% NGEPRINT TANGGAL
+format shortG
 
+disp('-------------------------------------------');
+disp('  PREDIKSI PRIMANTA TENTANG COVID DI INDO  ');
+disp('-------------------------------------------');
 
-%{
-plot (x5a,y5a);
-hold on;
-plot (x5b,y5b);
-hold on;
-plot (x5c,y5c);
-hold on;
-%}
+fprintf('Hari ini       --> '); disp(d6);
+fprintf('Puncak Active  --> '); disp(datestr((d3+datenum(d1,dForm)),dForm));
+fprintf('Akhir Lockdown --> '); disp(datestr((d4+datenum(d1,dForm)),dForm));
+fprintf('Akhir COVID    --> '); disp(datestr((d5+datenum(d1,dForm)),dForm));
+fprintf('\n');
+fprintf('Jumlah active cases maksimal  '); disp(sickPeak);
+fprintf('Jumlah positive cases maksimal'); disp(positivePeak);
+disp('nb:');
+disp('mekanisme perhitungan ini hanyalah prediksi');
+disp('         kritik dan saran sangat diharapkan');
+disp('-------------------------------------------');
+%% END OF CODE -----------------------------------------------------------
 
-%{
-%% GRAFIK CURED CASES
-e = 0.0011;
-f = 3.3418;
-
-x5a = [1:1:t]; %domain waktu dari awal sampai titik balik global
-y5a = e*x5a.^(f); %fungsi grafik cured naik
-
-t5b = ((y2b/e).^(1/f)); %titik dimana grafik mesti membalik
-t5bTuned = t5b + (0.57); %ditune dulu soalnya biasanya ada ketidaktelitian
-x5b = [t:1:t5bTuned]; %batas waktunya
-y5b = e*x5b.^(f); %grafiknya
-%{
-t5c = 2*t5b;
-x5c = [t5b:1:(t5c)];
-y5c = 2*m - (e*((2*t5b)-x5c).^(f));
-%}
-
-%}
-
-%{
-  disp('JADI, Covid mungkin paling cepet kelar tanggal');
-  disp(d3);
-  disp('MUNGKIN, hehe');
+%{    
+date3 = datenum(d1,dForm)+d3;
+    fullDate3 = datestr(d3,dForm);
+    disp(date3);
+    disp(fullDate3);
 %}
